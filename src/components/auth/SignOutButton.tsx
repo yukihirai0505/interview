2ea5266198dc8b6e3f9e93d069ede133f2332ca5
Yuki Hirai <yukihirai0505@gmail.com>
2019-08-29 17:36:35 +0900

    Fix logout

--- src/components/auth/SignOutButton.tsx
+++ src/components/auth/SignOutButton.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import { navigate } from 'gatsby'
 
 const SignOutButton = () => {
   const onClickHandler = () => {
     firebase.auth().signOut()
+    navigate('/')
   }
 
   return (
